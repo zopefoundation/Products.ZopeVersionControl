7835516a0b9a52635b51f606c93fbf41b16aeff9
Hanno Schlichting <hanno@hannosch.eu>
2010-08-04 20:18:35 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='Products.ZopeVersionControl',
-      version='1.1.1',
+      version='1.1.2',
       description="Zope Version Control",
       long_description=open("CHANGES.txt").read(),
       classifiers=[
