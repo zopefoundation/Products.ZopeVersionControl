ce31bb6732a479beb28e954be57e2289387bfc60
Tres Seaver <tseaver@palladion.com>
2012-06-18 15:15:26 +0000

    Repository policy.

--- Products/ZopeVersionControl/tests/test_all.py
+++ Products/ZopeVersionControl/tests/test_all.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2003 Zope Foundation and Contributors.
 #
 # This software is subject to the provisions of the Zope Public License,
-# Version 1.2 (ZPL).  A copy of the ZPL should accompany this
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this
 # distribution.
 #
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
