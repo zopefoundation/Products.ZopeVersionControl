32e6c108fd0f6286c1d8f056b6ea6f2b1b9526cc
Jürgen Gmach <juergen.gmach@apis.de>
2019-04-14 01:51:55 +0200

    Add support for Python 3.7 and 3.8.
    modified:   .travis.yml
    modified:   CHANGES.rst
    modified:   setup.py
    modified:   tox.ini

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     license='ZPL',
     author='Zope Foundation and Contributors',
