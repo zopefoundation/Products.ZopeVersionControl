f0475897981f0684f7d7128d5dffe72a74fea712
Jens Vagelpohl <jens@plyp.com>
2025-11-19 19:23:31 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Products.ZopeVersionControl',
-    version='5.1',
+    version='5.2.dev0',
     description="Zope Version Control",
     long_description=(open('README.rst').read() + "\n" +
                       open('CHANGES.rst').read()),
